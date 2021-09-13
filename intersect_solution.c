84fcb9b473b33d5ce45b4a8fe028ec758be01f84
Lucas Caramês <lgpcarames@gmail.com>
2021-09-13 14:42:03 -0300

    Adicionado o arquivo de teste pickmin_teste.c

--- intersect_solution.c
+++ intersect_solution.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#define RAND_MAX 2147483647.0
 
 
 #define X_OUT 1234.0
 #define L 50
 #define RV 1
+
+#define PI 3.14159265358979323846
 
 static float sqrarg;
 #define SQR(a) ((sqrarg=(a)) == 0.0 ? 0.0 : sqrarg*sqrarg)
@@ -42,24 +46,46 @@ void interval_solution(double x, double y, double xnew, double ynew, double rv, 
         delta_in = sqrt(discriminant_in);
         t1=(-b+delta_in)/(2*a);
         t2=(-b-delta_in)/(2*a);
-        if(t1<0){t1=X_OUT;}
-        if(t2<0){t2=X_OUT;}
+        //if(t1<0){t1=X_OUT;}
+        //if(t2<0){t2=X_OUT;}
     }
 
     if(discriminant_out>0){
         delta_out = sqrt(discriminant_out);
         t3=(-b+delta_out)/(2*a);
         t4=(-b-delta_out)/(2*a);
-        if(t3<0){t3=X_OUT;}
-        if(t4<0){t4=X_OUT;}
+        //if(t3<0){t3=X_OUT;}
+        //if(t4<0){t4=X_OUT;}
     }
 }
 
-
+float RandomFloat(float min, float max){
+   return ((max - min) * ((float)rand() / RAND_MAX)) + min;
+}
 
 int main(){
-	interval_solution(3.32, 0.79, 66.06, 22.49, 1, 50);
-	printf("t1=%lf, t2=%lf, t3=%lf, t4=%lf\n", t1, t2, t3, t4);
+
+    int i;
+    double x, y, xnew, ynew, cx, cy, rrx;
+    double phi=2*RandomFloat(0, 1)*PI;
+    double vx=cos(phi); 
+    double vy=sin(phi);
+    //double drand48();
+    for(i=0;i<15;i++){
+        rrx=RandomFloat(0.0, 5000.0);
+        x=2;
+        y=0;
+        xnew=x+rrx*vx;
+        ynew=y+rrx*vy;
+        interval_solution(x, y, xnew, ynew, RV, L);
+        printf("xnew=%lf, ynew=%lf\n", xnew, ynew);
+        printf("t1=%lf, t2=%lf, t3=%lf, t4=%lf\n", t1, t2, t3, t4);    
+
+    }
+
+
+//	interval_solution(3.32, 0.79, 66.06, 22.49, 1, 50);
+//	printf("t1=%lf, t2=%lf, t3=%lf, t4=%lf\n", t1, t2, t3, t4);
 
 	return 0;
 }
