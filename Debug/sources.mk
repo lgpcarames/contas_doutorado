57439ae2273ff36d23fd04244f78bc9215dd4333
Lucas <lgpcarames@gmail.com>
2021-10-17 14:40:16 -0300

    arquivos modificados na pasta debug

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -16,4 +16,12 @@ SUBDIRS := \
 .metadata/.plugins/org.eclipse.cdt.make.core \
 Rascunhos \
 . \
+venv/Lib/site-packages/numpy/distutils/checks \
+venv/Lib/site-packages/numpy/distutils/mingw \
+venv/Lib/site-packages/numpy/f2py/src \
+venv/Lib/site-packages/numpy/f2py/tests/src/array_from_pyobj \
+venv/Lib/site-packages/pandas/_libs/src/parser \
+venv/Lib/site-packages/pandas/_libs/src/ujson/lib \
+venv/Lib/site-packages/pandas/_libs/src/ujson/python \
+venv/Lib/site-packages/pandas/_libs/tslibs/src/datetime \
 
